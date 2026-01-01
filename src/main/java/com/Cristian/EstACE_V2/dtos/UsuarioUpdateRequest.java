fdec08a0d3f5c79af7a7ee0af87ebe36b2c02d2a
CCrisstian <crisstiann.c@gmail.com>
2026-01-01 20:24:34 -0300

    Estacionamiento - mejorado DTO

--- src/main/java/com/Cristian/EstACE_V2/dtos/UsuarioUpdateRequest.java
+++ src/main/java/com/Cristian/EstACE_V2/dtos/UsuarioUpdateRequest.java
@@ -2,6 +2,7 @@ package com.Cristian.EstACE_V2.dtos;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -12,7 +13,11 @@ public class UsuarioUpdateRequest {
     @Max(value = 99999999, message = "El DNI no puede tener más de 8 dígitos")
     private Integer dni;
 
+    // Regex: Solo letras, acentos y espacios
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "El nombre solo puede contener letras")
     private String nombre;
+
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "El apellido solo puede contener letras")
     private String apellido;
 
     // Dato exclusivo (solo Dueño puede editar su propia contraseña)
